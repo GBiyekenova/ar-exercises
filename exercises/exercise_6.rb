59cd6b442b12201b77e63e3aa63ea40e79fdc051
Gulbike Biyekenova <gbknva@gmail.com>
2022-01-04 16:50:52 +0000

    adds one-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+puts Store.has_many :employees
+puts Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessie", last_name: "Lang", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Adam", last_name: "Sing", hourly_rate: 60)
+@store2.employees.create(first_name: "Bob", last_name: "Adams", hourly_rate: 60)
